a57e5358f0009a1141e21c24d79209bd342a2d8d
Oleg Dolya <oleg.dolya@gmail.com>
2019-09-27 00:19:06 +0300

    modify build prod

--- .reaction/build-prod.sh
+++ .reaction/build-prod.sh
@@ -5,10 +5,24 @@ APP_BUNDLE_DIR=${APP_BUNDLE_DIR:-/opt/reaction/dist}
 
 export PATH=$PATH:/home/node/.meteor:${APP_SOURCE_DIR}/node_modules/.bin
 export HOME=/home/node
-export NODE_ENV=production
+
+# export NODE_ENV=production
 echo "USER: ${USER} NODE_ENV: ${NODE_ENV}"
 
 cd ${APP_SOURCE_DIR}
+
+# meteor npm install @babel/runtime simpl-schema react lodash imports \
+#  accounting-js i18next @reactioncommerce/reaction-error react-router-dom \
+#  react-router history path-to-regexp query-parse immutable \
+#  @reactioncommerce/reaction-components @reactioncommerce/logger \
+#  meteor-node-stubs transliteration slugify @reactioncommerce/file-collections \
+#  prop-types moment moment-timezone store @reactioncommerce/random \
+#  i18next-browser-languagedetector i18next-sprintf-postprocessor \
+#  i18next-fetch-backend i18next-multiload-backend-adapter jquery-i18next \
+#  @reactioncommerce/schemas @reactioncommerce/reaction-router 
+
+meteor npm install
+meteor npm install chalk
 
 node --experimental-modules ./.reaction/scripts/build.mjs
 echo "[-] Building Meteor application..."
@@ -17,5 +31,10 @@ meteor build --server-only --architecture os.linux.x86_64 --directory "${APP_BUN
 cd ${APP_BUNDLE_DIR}/bundle/programs/server/
 
 # meteor npm install --save @babel/runtime
-meteor npm install @babel/runtime
+# meteor npm install @babel/runtime
 meteor npm install --production
+
+# meteor npm install --save @babel/runtime react lodash imports accounting-js
+# │ │ │ meteor-artifact/setup  i18next @reactioncommerce/reaction-error react-router-dom react-router history
+# │ │ │ meteor-artifact/setup  path-to-regexp query-parse immutable @reactioncommerce/reaction-components
+# │ │ │ meteor-artifact/setup  @reactioncommerce/logger @reactioncommerce/random transliteration
