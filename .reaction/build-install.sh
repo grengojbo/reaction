78eae64ba29892a5e412275e91b069d82dc62fc8
Oleg Dolya <oleg.dolya@gmail.com>
2019-09-28 00:45:21 +0300

    fix bug

--- .reaction/build-install.sh
+++ .reaction/build-install.sh
@@ -3,8 +3,8 @@
 APP_SOURCE_DIR=${APP_SOURCE_DIR:-/opt/reaction/src}
 APP_BUNDLE_DIR=${APP_BUNDLE_DIR:-/opt/reaction/dist}
 
-#export PATH=$PATH:/home/node/.meteor:${APP_SOURCE_DIR}/node_modules/.bin
-export PATH=$PATH:${APP_SOURCE_DIR}/node_modules/.bin
+export PATH=$PATH:/home/node/.meteor:${APP_SOURCE_DIR}/node_modules/.bin
+# export PATH=$PATH:${APP_SOURCE_DIR}/node_modules/.bin
 export HOME=/home/node
 # export NODE_ENV=development
 
